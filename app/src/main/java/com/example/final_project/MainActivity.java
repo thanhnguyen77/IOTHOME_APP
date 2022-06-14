1571088adcc6f7b4a77279120bf656bb7ac4119b
Admin <thanhthanhnguyem79@gmail.com>
2022-06-14 07:50:52 +0700

    first

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -6,98 +6,215 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
-    ImageView imggas;
-    ImageView imglight;
-    TextView textnhietdo,textgas;
-    TextView textdoam ;
-    Button btnon;
-    Button btnoff;
+    private static final long START_TIME_IN_MILLIS=600000;
+    TextView textnhietdo,textdoam,textdoamdat;
+    Button btn1,btn2,btn3,btn4;
+    private EditText mEditTextInput;
+    private TextView mTextViewCountDown;
+    private Button mButtonSet;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private Spinner spinner;
+    private boolean mTimerRunning;
+
+    private long mStartTimeInMillis;
+    private long mTimeLeftInMillis;
+    private long mEndTime;
+    String device;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initui();
-        readData();
-        btnon.setOnClickListener(new View.OnClickListener() {
+        Event();
+        ArrayAdapter<String> myAdapter=new ArrayAdapter<String>(MainActivity.this,R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                OnLight();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String spinnerValue = spinner.getSelectedItem().toString();
+                device=spinnerValue;
+                System.out.println(spinnerValue);
+      //          Toast.makeText(MainActivity.this, spinnerValue, Toast.LENGTH_SHORT).show();
+  //              changeButton(spinnerValue);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
-        btnoff.setOnClickListener(new View.OnClickListener() {
+        setCountDownTime();
+
+
+    }
+
+    private void setCountDownTime() {
+        mButtonSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OffLight();
+                String input = mEditTextInput.getText().toString();
+                if (input.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                long millisInput = Long.parseLong(input) * 60000;
+                if (millisInput == 0) {
+                    Toast.makeText(MainActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                setTime(millisInput);
+                mEditTextInput.setText("");
             }
         });
-        
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+    }
+
+     void changeButton(String s) {
+
+        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+        DatabaseReference device1=firebaseDatabase.getReference("device1");
+        DatabaseReference device2=firebaseDatabase.getReference("device2");
+        DatabaseReference device3=firebaseDatabase.getReference("device3");
+        DatabaseReference device4=firebaseDatabase.getReference("device4");
+        if(s=="Device1") {
+
+            device1.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        Log.e("firebase", "Error getting data", task.getException());
+                    } else {
+                        Toast.makeText(MainActivity.this, String.valueOf(task.getResult().getValue()), Toast.LENGTH_SHORT).show();
+                        String s3 = String.valueOf(task.getResult().getValue());
+
+                           for(int i=0;i<10;i++){
+                               device1.setValue("0");
+                               device1.setValue("1");
+                           }
+
+
+                    }
+                }
+            });
+        }
+    //
+//           device1.addValueEventListener(new ValueEventListener() {
+//               @Override
+//               public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                   String value;
+//                   value = snapshot.getValue(String.class);
+//                   if(value.equals("1")){
+//                       device1.setValue("0");
+//                   }else  device1.setValue("1");
+//               }
+//
+//               @Override
+//               public void onCancelled(@NonNull DatabaseError error) {
+//
+//               }
+//           });
+
+
 
     }
 
     private void initui(){
-        textgas=findViewById(R.id.gas);
-        imggas=findViewById(R.id.khigas);
-        imglight=findViewById(R.id.light);
+
         textnhietdo=findViewById(R.id.nhietdo);
         textdoam=findViewById(R.id.doam);
-        btnon=findViewById(R.id.onLight);
-        btnoff=findViewById(R.id.offLight);
-    }
+        textdoamdat=findViewById(R.id.doamdat);
+        btn1=findViewById(R.id.device1);
+        btn2=findViewById(R.id.device2);
+        btn3=findViewById(R.id.device3);
+        btn4=findViewById(R.id.device4);
+        mEditTextInput = findViewById(R.id.edit_text_input);
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
 
-    private void OnLight() {
-        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-        DatabaseReference light=firebaseDatabase.getReference("led");
-        light.setValue("1");
-        imglight.setImageResource(R.drawable.light_on);
-
-
-    }
-    private void OffLight() {
-        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-        DatabaseReference light=firebaseDatabase.getReference("led");
-        light.setValue("0");
-        imglight.setImageResource(R.drawable.light_off);
-
+        mButtonSet = findViewById(R.id.button_set);
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+        spinner=findViewById(R.id.spinner);
 
     }
 
-
-
-
-
-
-
-
-    private void readData() {
+    private void Event() {
         FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-        DatabaseReference light=firebaseDatabase.getReference("led");
-        DatabaseReference gas=firebaseDatabase.getReference("gas");
-        DatabaseReference gascaution=firebaseDatabase.getReference("gascaution");
+        DatabaseReference device1=firebaseDatabase.getReference("device1");
+        DatabaseReference device2=firebaseDatabase.getReference("device2");
+        DatabaseReference device3=firebaseDatabase.getReference("device3");
+        DatabaseReference device4=firebaseDatabase.getReference("device4");
+        DatabaseReference doamdat=firebaseDatabase.getReference("doamdat");
         DatabaseReference doam=firebaseDatabase.getReference("doam");
         DatabaseReference nhietdo=firebaseDatabase.getReference("nhietdo");
 
-        light.addValueEventListener(new ValueEventListener() {
+
+        device1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value;
                 value = snapshot.getValue(String.class);
                 if(value.equals("1")){
-                    imglight.setImageResource(R.drawable.light_on);
-                }else imglight.setImageResource(R.drawable.light_off);
+                    btn1.setBackgroundResource(R.drawable.on);
+                }else  btn1.setBackgroundResource(R.drawable.off);
+                btn1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(value.equals("1")){
+                            device1.setValue("0");
+                        }else  device1.setValue("1");
+                    }
+                });
             }
 
             @Override
@@ -105,42 +222,45 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        gascaution.addValueEventListener(new ValueEventListener() {
+        device2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value;
                 value = snapshot.getValue(String.class);
                 if(value.equals("1")){
-                    imggas.setImageResource(R.drawable.cogas);
-                    AlertDialog alertDialog=new AlertDialog.Builder(MainActivity.this).create();
-                    alertDialog.setTitle("Cảnh báo phát hiện khí gas");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "ĐÃ NHẬN",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-
-                                }
-                            });
-                    alertDialog.show();
-
-
-                }else imggas.setImageResource(R.drawable.khonggas);
-
-            };
+                    btn2.setBackgroundResource(R.drawable.on);
+                }else  btn2.setBackgroundResource(R.drawable.off);
+                btn2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(value.equals("1")){
+                            device2.setValue("0");
+                        }else  device2.setValue("1");
+                    }
+                });
+            }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
-        gas.addValueEventListener(new ValueEventListener() {
+        device3.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value;
                 value = snapshot.getValue(String.class);
-                textgas.setText(value);
-
+                if(value.equals("1")){
+                    btn3.setBackgroundResource(R.drawable.on);
+                }else  btn3.setBackgroundResource(R.drawable.off);
+                btn3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(value.equals("1")){
+                            device3.setValue("0");
+                        }else  device3.setValue("1");
+                    }
+                });
             }
 
             @Override
@@ -148,6 +268,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        device4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value;
+                value = snapshot.getValue(String.class);
+                if(value.equals("1")){
+                    btn4.setBackgroundResource(R.drawable.on);
+                }else  btn4.setBackgroundResource(R.drawable.off);
+                btn4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(value.equals("1")){
+                            device4.setValue("0");
+                        }else  device4.setValue("1");
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         nhietdo.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -161,12 +305,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-                doam.addValueEventListener(new ValueEventListener() {
+        doam.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String value;
                 value = snapshot.getValue(String.class);
                 textdoam.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        doamdat.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value;
+                value = snapshot.getValue(String.class);
+                textdoamdat.setText(value);
             }
 
             @Override
@@ -181,10 +338,149 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//
-//    private void turnOnLed() {
-//        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-//        DatabaseReference myReference=firebaseDatabase.getReference("led");
-//        myReference.setValue("1");
-//    }
+    private void setTime(long milliseconds) {
+        mStartTimeInMillis = milliseconds;
+        resetTimer();
+        closeKeyboard();
+    }
+
+    private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+        //        Toast.makeText(MainActivity.this, device, Toast.LENGTH_SHORT).show();
+
+                updateWatchInterface();
+                changeButton("Device1");
+            }
+        }.start();
+
+        mTimerRunning = true;
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        updateWatchInterface();
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = mStartTimeInMillis;
+        updateCountDownText();
+        updateWatchInterface();
+    }
+
+    private void updateCountDownText() {
+        int hours = (int) (mTimeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((mTimeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%02d:%02d", minutes, seconds);
+        }
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+    private void updateWatchInterface() {
+        if (mTimerRunning) {
+            mEditTextInput.setVisibility(View.INVISIBLE);
+            mButtonSet.setVisibility(View.INVISIBLE);
+            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        } else {
+            mEditTextInput.setVisibility(View.VISIBLE);
+            mButtonSet.setVisibility(View.VISIBLE);
+            mButtonStartPause.setText("Start");
+
+            if (mTimeLeftInMillis < 1000) {
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+            } else {
+                mButtonStartPause.setVisibility(View.VISIBLE);
+            }
+
+            if (mTimeLeftInMillis < mStartTimeInMillis) {
+                mButtonReset.setVisibility(View.VISIBLE);
+            } else {
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("startTimeInMillis", mStartTimeInMillis);
+        editor.putLong("millisLeft", mTimeLeftInMillis);
+        editor.putBoolean("timerRunning", mTimerRunning);
+        editor.putLong("endTime", mEndTime);
+
+        editor.apply();
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+        mStartTimeInMillis = prefs.getLong("startTimeInMillis", 600000);
+        mTimeLeftInMillis = prefs.getLong("millisLeft", mStartTimeInMillis);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCountDownText();
+        updateWatchInterface();
+
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+                updateWatchInterface();
+            } else {
+                startTimer();
+            }
+        }
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/final_project/RegisterActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.final_project;
-
-import android.app.Activity;
-
-public class RegisterActivity extends Activity {
-}
